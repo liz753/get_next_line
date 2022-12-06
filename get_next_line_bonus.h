f52d7f14a440b456efb064e9d5a162dab7f8cafc
Lisa Frank <114035440+liz753@users.noreply.github.com>
2022-12-06 16:45:53 -0500

    Bonus added

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfrank <lfrank@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 14:45:46 by lfrank            #+#    #+#             */
+/*   Updated: 2022/12/06 13:57:47 by lfrank           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_read_and_stash(char *stash, int fd, int *nbrc);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+size_t	ft_strlen_n(char *s);
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
+char	*ft_get_and_free_stash(char **stash, int len, int set_stash_null);
+
+#endif
